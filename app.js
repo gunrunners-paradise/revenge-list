c561d6ca5a6581d5b08b7a8d7350d8a8bd61b43e
Oleg Cherkasky <old_juggernaut@hotmail.com>
2026-06-30 14:59:35 +0300

    organized a bit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 
+// routers
+const viewRoutes = require('./routes/view');
+
+// ejs view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// routes
 app.use(express.static('./public'));
+app.use('/',viewRoutes);
 
-app.get('/', (req,res) => {
-      res.render('index', {
-            title: 'Revenge App'
-      });
-});
-
+// server initialization
 const port = 3001;
 
 app.listen(port, () => {
---
+++ controllers/view.js
@@ -0,0 +1,7 @@
+const index = (req,res) => {
+      res.render('index', {
+            title: 'Revenge App'
+      });
+};
+
+module.exports = {index};
